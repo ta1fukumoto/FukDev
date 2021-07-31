adf0277a05763efbe12197a0d981e9e8a94c12a6
ta1fukumoto <fukumoto.t.1120@gmail.com>
2021-07-31 16:54:46 +0900

    updata: Add generateTableOfContent function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import fs from "fs"
 import { join } from "path"
 import matter from "gray-matter"
+import Parser from "rss-parser"
+import cheerio from "cheerio"
 
 const postsDirectory = join(process.cwd(), "_posts")
 
@@ -44,4 +46,44 @@ export const getAllPosts = (fields: string[] = []) => {
     // 投稿を日付の降順で並び替える
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
+}
+
+export const getRSSFeed = async (siteName: string, url: string) => {
+  const parser = new Parser()
+  const feed = await parser.parseURL(url)
+
+  const feedData = feed.items.map((data: any) => ({
+    slug: data.link,
+    title: data.title,
+    date: data.isoDate,
+    emoji: "",
+    category: siteName,
+  }))
+
+  return feedData
+}
+
+export const generateTableOfContent = (body: string) => {
+  const $ = cheerio.load(body, { decodeEntities: false })
+  let generateHtml = ""
+  generateHtml = generateHtml + "<ul>"
+  $("h1, h2").each((index, elm) => {
+    //h1 or h2タグの中身を取得し不要な部分を置換
+    const innerHeading = $(elm).html()
+    const text = innerHeading?.replace(/<a .+<\/a>/g, "")
+
+    //h1 or h2のタグの名前を取得
+    const tag = $(elm)[0].name
+
+    //idを取得
+    const refId = $(elm)[0].attribs.id
+
+    generateHtml =
+      generateHtml +
+      `<li class="toc_${tag}" key=${index}>` +
+      `  <a href="#${refId}">${text}</a>` +
+      "</li>"
+  })
+  generateHtml = generateHtml + "</ul>"
+  return generateHtml
 }
